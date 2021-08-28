198bef8c40a5a33bf9fd4d7836f804d0a81bd894
KesThav <kesigan.thav@gmail.com>
2021-08-28 18:09:36 +0200

    delete/merge rows,columns and na

--- server.R
+++ server.R
@@ -1,33 +1,115 @@
 library(DT)
 library(dplyr)
+library(shinyjs)
+
 
 shinyServer(function(input,output,session){
   
-  output$message <- renderText({
-    if(is.null(input$file)){
-      paste("Upload a file")
+
+
+  data <- reactiveVal()
+
+  listen <- reactive({list(input$file,input$na,input$delete,input$merge,input$delete_r)})
+  
+  observeEvent(input$file,{
+    if(!is.null(input$file)){
+      print(input$file)
+      file <- input$file
+      d <- read.csv(file$datapath,header=TRUE)
+      rownames(d) <- NULL
+      data(d)
     }
   })
   
-  listen <- reactive({list(input$file,input$na)})
   observeEvent(listen(),{
+    result <- data()
     if(!is.null(input$file)){
-      file <- input$file
-      dataset <- read.csv(file$datapath,header=TRUE)
-      output$display_file <- DT::renderDataTable({dataset})
-      
-      
-      output$str <- renderPrint({str(dataset)})
-      output$desc <-renderPrint({psych::describe(dataset)})
-      
+      output$display_file <- DT::renderDataTable({
       if(input$na){
-        data <- dataset %>% tidyr::drop_na()
-        output$display_file <- DT::renderDataTable({data})
-        output$str <- renderPrint({str(data)})
-        output$desc <-renderPrint({psych::describe(data)})
+        result <- result %>% tidyr::drop_na()
+        output$display_file <- DT::renderDataTable({result})
+        output$str <- renderPrint({str(result)})
+        output$desc <-renderPrint({psych::describe(result)})
+        data(result)
       }
+      
+      
+      else if(length(input$delete_col) != 0 & input$delete){
+        result <- as.data.frame(result[,-which(names(result) %in% input$delete_col)])
+        data(result)
+        output$display_file <- DT::renderDataTable({result})
+      }
+        
+      else if(length(input$delete_row) != 0 & input$delete_r){
+        for(row in input$delete_row){
+          result <- result[-as.numeric(row),]
+          print(row)
+        }
+        data(result)
+      }
+
+      
+      else if(input$merge & input$merge_col_name != ""){
+        result[,input$merge_col_name] <- NA
+        for(name in input$merge_col){
+          result[,input$merge_col_name] <- paste(result[,input$merge_col_name], result[,name])
+        }
+        result <- as.data.frame(result[,-which(names(result) %in% input$merge_col)])
+        data(result)
+        output$display_file <- DT::renderDataTable({result})
+
+        
+      }else{
+        output$display_file <- DT::renderDataTable({result})
+        output$str <- renderPrint({str(result)})
+        output$desc <-renderPrint({psych::describe(result)})
+
+      }
+      })
+
+      
+
     }
   })
+  
+  observeEvent(data(),{
+    
+  output$na <- renderUI({
+    checkboxInput("na","Drop na")
+  })
+  output$merge_col <- renderUI({
+    selectInput("merge_col","Select columns to merge",choices=colnames(data()), multiple=TRUE)
+    })
+  output$delete_col <- renderUI({
+    selectInput("delete_col","Select columns to delete",choices=colnames(data()),multiple=TRUE)
+    })
+
+  
+  output$merge_col_name <- renderUI({
+    textInput("merge_col_name","Enter merge name")
+  })
+  
+  output$delete <- renderUI({
+    actionButton("delete","delete columns")
+  })
+  
+  output$merge <- renderUI({
+    actionButton("merge","merge columns")
+  })
+  
+  output$delete_row <- renderUI({
+    selectInput("delete_row","Select rows to delete",choices=as.numeric(rownames(data())),multiple=TRUE)
+  })
+  
+  output$delete_r <- renderUI({
+    actionButton("delete_r","delete rows")
+  })
+  
+  
+  
+  })
+  
+
   
   
   
--- ui.R
+++ ui.R
@@ -1,30 +1,50 @@
 library(shiny)
 library(shinydashboard)
 library(DT)
+library(shinyjs)
 
 
 shinyUI(dashboardPage(
                   dashboardHeader(title="Pre-process dataset without coding"),
                   dashboardSidebar(
+                                  sidebarMenu(
+                                    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"),
+                                             uiOutput("na"),
+                                             uiOutput("delete_col"),
+                                             uiOutput("delete"),
+                                             uiOutput("merge_col"),
+                                             uiOutput("merge_col_name"),
+                                             uiOutput("merge"),
+                                             uiOutput("delete_row"),
+                                             uiOutput("delete_r")),
+                                             
+                                    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+                                             badgeLabel = "new", badgeColor = "green")
+                                  ),
+                    
                                   fileInput("file","Upload CSV file",multiple=FALSE,accept = c("text/csv",
                                                                                   "text/comma-separated-values,text/plain",
-                                                                                  ".csv")),
-                                  checkboxInput("na","Drop na")
+                                                                                  ".csv"))
+
+                                  
                                   ),
                   dashboardBody(
+                    
+                                useShinyjs(),
                                 fluidRow(
-                                        box(width=12,style="overflow-x : scroll",
-                                            column(width=12,textOutput("message"),
+                                        box(id="notif", width=12, style="backgroud: #ccff90", "Upload a file"),
+                                        box(id="data_table",width=12,style="overflow-x : scroll",
+                                            column(width=12,
                                                             DT::dataTableOutput("display_file")
                                                    )
                                             )  
                                         ),
-                                        box(width=12,
+                                        box(id="str_table",width=12,
                                             column(width=12,verbatimTextOutput("str")
                                               
                                                   )
                                             ),
-                                        box(width=12,
+                                        box(id="desc_table",width=12,
                                             column(width=12,verbatimTextOutput("desc")
                                            
                                                   )

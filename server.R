7e6923175f85c6016ac59308541e05e27dc957e2
KesThav <kesigan.thav@gmail.com>
2021-08-29 13:45:18 +0200

    more options add

--- server.R
+++ server.R
@@ -1,7 +1,9 @@
 library(DT)
 library(dplyr)
 library(shinyjs)
+library(shinyWidgets)
 
+jscode <- "shinyjs.refresh = function() { history.go(0); }"
 
 shinyServer(function(input,output,session){
   
@@ -9,20 +11,31 @@ shinyServer(function(input,output,session){
 
   data <- reactiveVal()
 
-  listen <- reactive({list(input$file,input$na,input$delete,input$merge,input$delete_r)})
+  listen <- reactive({list(input$file,input$na,input$start,input$delete,input$merge,input$delete_r,input$reset,input$convert_val,input$convert_type,input$col_to_convert)})
   
   observeEvent(input$file,{
     if(!is.null(input$file)){
-      print(input$file)
+      print("yes")
+      req(input$file)
       file <- input$file
       d <- read.csv(file$datapath,header=TRUE)
       rownames(d) <- NULL
-      data(d)
+      data(as.data.frame(d))
     }
   })
   
+  observeEvent(input$reset,{
+    file <- input$file
+    d <- read.csv(file$datapath,header=TRUE)
+    rownames(d) <- NULL
+    data(as.data.frame(d))
+  })
+
+
+  
   observeEvent(listen(),{
     result <- data()
+
     if(!is.null(input$file)){
       output$display_file <- DT::renderDataTable({
       if(input$na){
@@ -42,12 +55,11 @@ shinyServer(function(input,output,session){
         
       else if(length(input$delete_row) != 0 & input$delete_r){
         for(row in input$delete_row){
-          result <- result[-as.numeric(row),]
+          result <- result[-as.integer(c(row)),]
           print(row)
         }
         data(result)
       }
-
       
       else if(input$merge & input$merge_col_name != ""){
         result[,input$merge_col_name] <- NA
@@ -58,6 +70,28 @@ shinyServer(function(input,output,session){
         data(result)
         output$display_file <- DT::renderDataTable({result})
 
+      }else if(length(input$col_to_convert) != 0 & length(input$convert_type) != 0 & input$convert_val){
+        
+        if(input$convert_type == 'integer'){
+          print(input$convert_type)
+          result[,input$col_to_convert] <- as.integer(result[,input$col_to_convert])
+          data(result)
+        
+        }else if(input$convert_type == 'double'){
+          print(input$convert_type)
+          result[,input$col_to_convert] <- as.double(result[,input$col_to_convert])
+          data(result)
+          
+        }else if(input$convert_type == 'chr'){
+          print(input$convert_type)
+          result[,input$col_to_convert] <- as.character(result[,input$col_to_convert])
+          data(result)
+          
+        }else{
+          output$display_file <- DT::renderDataTable({result})
+        }
+        
+     
         
       }else{
         output$display_file <- DT::renderDataTable({result})
@@ -65,28 +99,37 @@ shinyServer(function(input,output,session){
         output$desc <-renderPrint({psych::describe(result)})
 
       }
-      })
 
-      
-
+  })
+    }
+},ignoreNULL=TRUE, ignoreInit=TRUE)
+  
+  observeEvent(data(), {
+    
+    if(length(data()) != 0){
+      shinyjs::hide(id = "notif")
+      shinyjs::show(id="hidden")
+    }else{
+      shinyjs::show(id = "notif")
+      shinyjs::hide(id="hidden")
     }
   })
   
   observeEvent(data(),{
     
   output$na <- renderUI({
-    checkboxInput("na","Drop na")
+    prettyCheckbox("na","Drop na")
   })
   output$merge_col <- renderUI({
-    selectInput("merge_col","Select columns to merge",choices=colnames(data()), multiple=TRUE)
+    pickerInput("merge_col",label="Select columns to merge",choices=colnames(data()), multiple=TRUE)
     })
   output$delete_col <- renderUI({
-    selectInput("delete_col","Select columns to delete",choices=colnames(data()),multiple=TRUE)
+    pickerInput("delete_col",label="Select columns to delete",choices=colnames(data()),multiple=TRUE)
     })
 
   
   output$merge_col_name <- renderUI({
-    textInput("merge_col_name","Enter merge name")
+    textInput("merge_col_name","Enter merge column's name")
   })
   
   output$delete <- renderUI({
@@ -98,15 +141,35 @@ shinyServer(function(input,output,session){
   })
   
   output$delete_row <- renderUI({
-    selectInput("delete_row","Select rows to delete",choices=as.numeric(rownames(data())),multiple=TRUE)
+    pickerInput("delete_row","Select rows to delete",choices=as.numeric(rownames(data())),multiple=TRUE)
   })
   
   output$delete_r <- renderUI({
     actionButton("delete_r","delete rows")
   })
   
+  output$col_to_convert <- renderUI({
+    pickerInput("col_to_convert","Select column to convert",choices=colnames(data()))
+  })
   
+  output$convert_type <- renderUI({
+    pickerInput("convert_type","Select what to convert to",choices=c('integer','double','chr'))
+  })
   
+  output$convert_val <- renderUI({
+    actionButton("convert_val","Convert")
+  })
+  
+  output$show_unique <- renderUI({
+    pickerInput("show_unique","Show unique",choices=colnames(data()),select=colnames(data())[1])
+  })
+
+  
+  })
+  
+  observeEvent(input$show_unique,{
+    result <- data()
+    output$unique <- renderPrint({unique(result[,input$show_unique])})
   })
   
 
--- ui.R
+++ ui.R
@@ -6,50 +6,66 @@ library(shinyjs)
 
 shinyUI(dashboardPage(
                   dashboardHeader(title="Pre-process dataset without coding"),
-                  dashboardSidebar(
+                  dashboardSidebar(width = 350,
+                                   useShinyjs(),
                                   sidebarMenu(
-                                    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"),
-                                             uiOutput("na"),
-                                             uiOutput("delete_col"),
-                                             uiOutput("delete"),
-                                             uiOutput("merge_col"),
-                                             uiOutput("merge_col_name"),
-                                             uiOutput("merge"),
-                                             uiOutput("delete_row"),
-                                             uiOutput("delete_r")),
+                                    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
                                              
-                                    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+                                    menuItem("Analyze", icon = icon("th"), tabName = "analyze",
                                              badgeLabel = "new", badgeColor = "green")
                                   ),
                     
                                   fileInput("file","Upload CSV file",multiple=FALSE,accept = c("text/csv",
                                                                                   "text/comma-separated-values,text/plain",
                                                                                   ".csv"))
-
                                   
                                   ),
                   dashboardBody(
-                    
-                                useShinyjs(),
-                                fluidRow(
-                                        box(id="notif", width=12, style="backgroud: #ccff90", "Upload a file"),
-                                        box(id="data_table",width=12,style="overflow-x : scroll",
-                                            column(width=12,
-                                                            DT::dataTableOutput("display_file")
-                                                   )
-                                            )  
-                                        ),
-                                        box(id="str_table",width=12,
-                                            column(width=12,verbatimTextOutput("str")
-                                              
-                                                  )
-                                            ),
-                                        box(id="desc_table",width=12,
-                                            column(width=12,verbatimTextOutput("desc")
-                                           
-                                                  )
-                                            )
+                                tabItems(
+                                  tabItem(tabName="dashboard",
+                                        fluidRow(
+                                              box(solidHeader = TRUE,id="notif", width=12, style="backgroud: #ccff90", h1("Upload a file")),
+                                              hidden(
+                                                    div(id="hidden",
+                                                      box(solidHeader = TRUE,id="menu",width=4,
+                                                          tabBox(width=12,
+                                                                  tabPanel("Handle columns",
+                                                                           uiOutput("delete_col"),
+                                                                           uiOutput("delete"),
+                                                                           uiOutput("merge_col"),
+                                                                           uiOutput("merge_col_name"),
+                                                                           uiOutput("merge")),
+                                                                  tabPanel("Handle rows",
+                                                                           uiOutput("delete_row"),
+                                                                           uiOutput("delete_r")),
+                                                                  tabPanel("Convert types",
+                                                                           uiOutput("col_to_convert"),
+                                                                           uiOutput("convert_type"),
+                                                                           uiOutput("convert_val")),
+                                                                  tabPanel("More options",
+                                                                           uiOutput("na"),
+                                                                           actionButton("reset","Reset"))
+                                                            
+                                                                  )
+                                                  
+                                                      ),
+                                                    box(solidHeader = TRUE,id="data_table",width=8,style="overflow-x : scroll",
+                                                          column(width=12,DT::dataTableOutput("display_file"))),
+                                                      
+                                                    box(solidHeader = TRUE,title="details",id="str_table",width=6,
+                                                    column(width=12,verbatimTextOutput("str"))),
+                                                    
+                                                    box(solidHeader = TRUE,title="describe()",id="desc_table",width=6,
+                                                    column(width=12,verbatimTextOutput("desc"))),
+                                                    
+                                                    box(solidHeader = TRUE,title="Show unique",id="unique_",width=6,
+                                                    column(width=12,uiOutput("show_unique")),
+                                                    column(width=12,verbatimTextOutput("unique")))
+                                          )))),
+                                tabItem("analyze",h1("Analyze"))
+
+
                                 
                                 )
           
-))
+)))
